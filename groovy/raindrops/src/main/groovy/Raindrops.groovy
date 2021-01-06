3c79d66697176b49ec864b966df8fb1f89034d0d
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-01-06 20:58:36 +0100

    raindrops: first solution.

--- groovy/raindrops/src/main/groovy/Raindrops.groovy
+++ groovy/raindrops/src/main/groovy/Raindrops.groovy
@@ -7,6 +7,8 @@ class Raindrops {
             ret+= num.toString()
         } else {
             if ( isDivisibleByThree(num) ) ret += "Pling"
+            if ( isDivisibleByFive(num) ) ret += "Plang"
+            if ( isDivisibleBySeven(num) ) ret += "Plong"
         }
 
         ret
@@ -16,6 +18,12 @@ class Raindrops {
 
     Boolean isDivisibleByThree(num) {
         num % 3 == 0
+    }
+    Boolean isDivisibleByFive(num) {
+        num % 5 == 0
+    }
+    Boolean isDivisibleBySeven(num) {
+        num % 7 == 0
     }
     Boolean isDivisableByThreeFiveOrSeven(num) {
         isDivisibleByThree(num) || num % 5 == 0 || num % 7 == 0
